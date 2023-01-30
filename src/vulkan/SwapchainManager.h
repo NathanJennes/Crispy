0cc9f46f0b320afaf530316085c6244464bcbea8
nathan <nathanjsim42@gmail.com>
2023-01-30 16:10:41 +0100

    SwapchainManager: Creating a depth buffer image
    This will be later used to setup depth-testing.

--- src/vulkan/SwapchainManager.h
+++ src/vulkan/SwapchainManager.h
@@ -7,6 +7,8 @@
 
 #include <vulkan/vulkan.h>
 #include <vector>
+#include <optional>
+#include "defines.h"
 
 namespace Vulkan {
 
@@ -39,6 +41,7 @@ public:
 	static std::vector<VkImageView>&	swapchain_image_views()		{ return _swapchain_image_views; }
 	static std::vector<VkFramebuffer>&	swapchain_framebuffers()	{ return _swapchain_framebuffers; }
 	static VkFormat&					swapchain_image_format()	{ return _swapchain_image_format; }
+	static VkFormat						find_depth_format();
 
 private:	// Types
 	struct SwapchainSupportDetails
@@ -57,6 +60,12 @@ private:	// Methods
 	static VkSurfaceFormatKHR		choose_surface_format(const std::vector<VkSurfaceFormatKHR>& available_formats);
 	static VkPresentModeKHR			choose_present_mode(const std::vector<VkPresentModeKHR>& available_modes);
 	static VkExtent2D				choose_swap_extent(const VkSurfaceCapabilitiesKHR& capabilities);
+	static VkFormat					choose_supported_format(const std::vector<VkFormat>& formats, VkImageTiling tiling, VkFormatFeatureFlags features);
+	static bool						has_stencil_component(VkFormat format);
+	static bool						create_image(u32 width, u32 height, VkFormat format, VkImageTiling tiling,
+												VkImageUsageFlags usage, VkMemoryPropertyFlags memory_property,
+												VkImage *out_image, VkDeviceMemory *out_image_memory);
+	static std::optional<u32>		find_memory_type(u32 type_filter, VkMemoryPropertyFlags properties);
 
 	//----
 	// Swapchain creation and destruction
@@ -65,9 +74,10 @@ private:	// Methods
 	static void						cleanup_swapchain();
 
 	//----
-	// Images
+	// Attachments
 	//----
 	static void	create_image_views();
+	static void	create_depth_resources();
 
 private:	// Members
 	static VkSwapchainKHR				_swapchain;
@@ -76,6 +86,10 @@ private:	// Members
 	static std::vector<VkFramebuffer>	_swapchain_framebuffers;
 	static VkFormat						_swapchain_image_format;
 	static VkExtent2D					_swapchain_extent;
+
+	static VkImage			depth_image;
+	static VkDeviceMemory	depth_image_memory;
+	static VkImageView		depth_image_view;
 };
 }
 
